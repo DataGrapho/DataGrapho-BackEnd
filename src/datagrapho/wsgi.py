fa3a2fc21ba42cd346e68f5d721a6a70118761a7
Alexsanjr <alex.sandro.1199@gmail.com>
2026-04-30 06:01:43 -0300

    Ajuste realizado #13

--- src/datagrapho/wsgi.py
+++ src/datagrapho/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+src_path = Path(__file__).resolve().parent.parent
+if str(src_path) not in sys.path:
+    sys.path.insert(0, str(src_path))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datagrapho.settings')
 
