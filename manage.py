fcea9237d12841aa54f3621c8a80aac11fe969a4
Alexsanjr <alex.sandro.1199@gmail.com>
2026-04-30 06:01:43 -0300

    Ajuste realizado #13

--- manage.py
+++ manage.py
@@ -2,10 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
+    src_path = Path(__file__).resolve().parent / 'src'
+    if str(src_path) not in sys.path:
+        sys.path.insert(0, str(src_path))
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datagrapho.settings')
     try:
         from django.core.management import execute_from_command_line
